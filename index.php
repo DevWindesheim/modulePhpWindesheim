40b38c60f1b643081c4b4aea226d72354ba2c15d
Stephan Paul <ginstephan@gmail.com>
2017-04-11 13:00:50 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by: Stephan Hoeksema 2017
+ * OOP
+ */
+$name = 'stephan';
+$age = 40;
+$greeting = 'Hallo ADSD 2017/18 ';
+
+
+echo $greeting;
+//echo "<br>";
+//echo $greeting . $name . ' ' . $age;
+//echo "<br>";
+//echo $greeting . $_GET['name'] . ' ' . $_GET['age'];
+//echo "<br>";
+//echo $greeting . ucfirst($_GET['name']) . ' ' . htmlspecialchars($_GET['age']);
+//echo "<br>";
